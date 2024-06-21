038afba911847369c4ac56a7c687e99410f3117c
UrasAydn <urastalhaarzu@gmail.com>
2024-06-21 22:09:11 +0300

    Update sozluk.py

--- sozluk.py
+++ sozluk.py
@@ -1,12 +1,13 @@
 meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
-            "MVP":'En değerli oyuncu'
+            "MVP":"En değerli oyuncu"
         
             } 
- kelime=input ('kelime girin') 
- 
- if kelime in meme_dict.keys():
+ for i in range(5)
+    kelime=input ('kelime girin') 
+            
+    if kelime in meme_dict.keys():
      print(meme_dict[kelime])
-else
-    print('kelime listede yok')
+    else
+     print('kelime listede yok')
